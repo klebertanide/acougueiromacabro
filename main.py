fc353e51ea6be9c0f47b913a93e15319eac6996c
klebertanide <klebertanide@gmail.com>
2025-05-25 20:26:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -216,6 +216,11 @@ def transcrever():
         try: fobj.close()
         except: pass
 
+@app.route("/", methods=["GET"])
+def index():
+    return "Servidor do Açougueiro Macabro está online e rodando."
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
